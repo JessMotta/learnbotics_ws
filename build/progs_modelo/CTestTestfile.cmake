a90f38045004b820283319cfaa489bfc4410d867
JessMotta <jessicalimamotta@gmail.com>
2020-01-10 16:53:36 -0300

    first commit

---
+++ build/progs_modelo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/jess/learnbotics_ws/src/progs_modelo
+# Build directory: /home/jess/learnbotics_ws/build/progs_modelo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
